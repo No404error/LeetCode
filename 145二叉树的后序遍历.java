f0baabaec55e51871994d514ff24abc928cbc3ff
Zhang <3484868205@qq.com>
2023-05-23 10:40:16 +0800

    LeetCode Binary Tree traverse

---
+++ 145二叉树的后序遍历.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        if(root!=null)
+            traverse(list,root);
+        return list;
+    }
+    public void traverse(List<Integer> list,TreeNode root){
+        if(root.left!=null) traverse(list,root.left);
+        if(root.right!=null) traverse(list,root.right);
+        list.add(root.val);
+    }
+}
