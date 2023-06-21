93c353dbe4c3b9a97eb0eb9de7083adffd5d7625
Zhang <3484868205@qq.com>
2023-06-21 13:55:38 +0800

    binary search

--- Solution.java
+++ Solution.java
@@ -1,39 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int add=0;
-        ListNode root=new ListNode();
-        ListNode walk=root;
-        while(l1!=null||l2!=null){
-            walk.next=new ListNode();
-            walk=walk.next;
-            int val=0;
-            if(l1!=null&&l2!=null){
-                val=l1.val+l2.val+add;
-                l1=l1.next;
-                l2=l2.next;
-            }else if(l1!=null){
-                val=l1.val+add;
-                l1=l1.next;
-            }else{
-                val=l2.val+add;
-                l2=l2.next;
-            }
-            add=val/10;
-            val=val%10;
-            walk.val=val;
-        }
-        if(add!=0)
-        walk.next=new ListNode(add);
-        return root.next;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len=nums1.length+nums2.length;
+        int left=(len+1)/2;
+        int right=(len+2)/2;
+        return (getKth(nums1,0,nums1.length,nums2,0,nums2.length,left)+getKth(nums1,0,nums1.length,nums2,0,nums2.length,right))/2.0;
+    }
+
+    int getKth(int[] a,int as,int ae,int[] b,int bs,int be,int k){
+        int len1=ae-as;
+        int len2=be-bs;
+        if(len1>len2)
+            return getKth(b,bs,be,a,as,ae,k);
+        if(len1==0)
+            return b[bs+k-1];
+        else if(k==1)
+            return Math.min(a[as],b[bs]);
+
+        int act1=as+Math.min(len1,k/2)-1;
+        int act2=bs+Math.min(len2,k/2)-1;
+
+        if(a[act1]>b[act2]){
+            return getKth(a,as,ae,b,act2+1,be,k-(act2-bs+1));
+        }else
+            return getKth(a,act1+1,ae,b,bs,be,k-(act1-as+1));
+
     }
 }
